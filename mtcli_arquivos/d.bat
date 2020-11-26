8861126a7df6da884563ea51ec8006d8521d7a94
Valmir Franca <vfranca3@gmail.com>
2020-11-26 05:37:38 -0300

    Atualizado bat que altera o date do terminal de comandos

--- mtcli_arquivos/d.bat
+++ mtcli_arquivos/d.bat
@@ -1,9 +1,28 @@
 @echo off
 @rem aliases para comandos do mtcli
-if "%1" == "" (
-set d=""
-echo date %d%
+set a=2020
+set m=11
+
+if "%1" == "-h" (
+echo d [dia] [mes] [ano]
+echo vazio - nao define nenhum intraday
+echo dia - define o dia do mes
+echo dia mes ano define uma data especifica
 goto :EOF
 )
+
+
+if "%1" == "" (
+set d=""
+echo data intraday vazia
+goto :EOF
+)
+
+if "%2" == "" (
+set d=%a%.%m%.%1
+echo data intraday %a%.%m%.%1
+goto :EOF
+)
+
 set d=%3.%2.%1
-echo date %d%
+echo data intraday %3.%2.%1
