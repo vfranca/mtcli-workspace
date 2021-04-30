359a5e38bde25ccddcb8105c6d9451857dc8da5a
Valmir Franca <vfranca3@gmail.com>
2021-04-30 04:33:12 -0300

    Adicionados novos atalhos para perfis: para bgi e generico

---
+++ mtcli/bgi.bat
@@ -0,0 +1,9 @@
+@echo off
+rem mtcli workspace
+rem perfil BGI
+set prefix=bgi
+set /p sufix=sufixo:
+rem set sufix=k21
+set digits=2
+rem set mt5=clear
+call perfil
---
+++ mtcli/pf.bat
@@ -0,0 +1,8 @@
+@echo off
+rem mtcli workspace
+rem perfil ação
+set /p prefix=prefixo:
+set /p sufix=sufixo:
+set digits=2
+rem set mt5=clear
+call perfil
