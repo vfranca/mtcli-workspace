15b75e3192154e5922fec6230741d7fe22167a5d
Valmir Franca <vfranca3@gmail.com>
2021-04-30 04:31:55 -0300

    Alterados atalhos para medias moveis e adicionado atalho para mm 200 do diario

--- mtcli/mm200.bat
+++ mtcli/mm200.bat
@@ -1,9 +1,9 @@
 @echo off
 rem Aliases para comandos do mtcli
-rem médias móveis de 9 e 20 períodos
+rem médias móveis de 20 e 200 períodos
 set symbol=%T%
-set mm_curta=9
-set mm_longa=20
+set mm_curta=20
+set mm_longa=200
 mt bars %symbol% -p daily -v c -c 1
 sma %symbol% -p daily -c %mm_curta%
 sma %symbol% -p daily -c %mm_longa%
--- mtcli/mmd.bat
+++ mtcli/mmd.bat
@@ -1,11 +1,9 @@
 @echo off
 rem Aliases para comandos do mtcli
 rem Médias móveis do diário
-rem entradas
 set curta=20
 set inter=50
-set longa=150
-rem ajuda
+set longa=200
 if "%1" == "-h" (
 echo medias moveis do diario
 echo fechamento
@@ -14,7 +12,6 @@ echo mm%inter%
 echo mm%longa%
 goto :EOF
 )
-rem comandos
 mt bars %t% --period daily --view c --count 1
 sma %t% --period daily --count %curta%
 sma %t% --period daily --count %inter%
