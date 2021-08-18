1e3f1506d57106db4edfadcf9a4b9b084a3bf05a
Valmir Franca <vfranca3@gmail.com>
2021-08-18 03:30:00 -0300

    Reformulado mt5 para alterar o mt5 do mtcli

--- mtcli/mt5.bat
+++ mtcli/mt5.bat
@@ -1,53 +1,7 @@
 @echo off
 rem workspace do mtcli
-rem Altera o MT5 do terminal
-rem variaveis de entrada
+rem Altera o MT5 do mtcli
 set envdir=C:\
-set mt5="%1"
-rem ajuda
-if "%1" == "" (
-echo altera o MetaTrader do terminal
-echo Digite uma das seguintes opcoes:
-echo b3 ou clear - Para MetaTrader da Clear
-echo clear2 - Para MetaTrader da Clear demo
-echo icm - Para MetaTrader da Ic Markets
-echo xm - Para MetaTrader da XM
-echo xm2 - Para MetaTrader da XM demo
-echo at - Para MetaTrader da ActivTrades
-goto :EOF
-)
-rem comandos
-rem Seleciona MT5 da Clear
-if %mt5% == "b3" (
-copy %envdir%.env-clear %envdir%.env
-goto :EOF
-)
-if %mt5% == "clear" (
-copy %envdir%.env-clear %envdir%.env
-goto :EOF
-)
-rem Seleciona MT5 da Clear demo
-if %mt5% == "clear2" (
-copy %envdir%.env-clear-demo %envdir%.env
-goto :EOF
-)
-rem Seleciona MT5 da IC Markets
-if %mt5% == "icm" (
-copy %envdir%.env-icm %envdir%.env
-goto :EOF
-)
-rem Seleciona MT5 da XM
-if %mt5% == "xm" (
-copy %envdir%.env-xm %envdir%.env
-goto :EOF
-)
-rem Seleciona MT5 da XM demo
-if %mt5% == "xm2" (
-copy %envdir%.env-xm-demo %envdir%.env
-goto :EOF
-)
-rem Seleciona MT5 da ActivTrades
-if %mt5% == "at" (
-copy %envdir%.env-activ %envdir%.env
-goto :EOF
-)
+rem set mt5="%1"
+set /p mt5=corretora:
+copy %envdir%.env-%mt5% %envdir%.env
