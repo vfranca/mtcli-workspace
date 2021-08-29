e512c763d7ed9d0ded6ed703d7d781b3c5b57063
Valmir Franca <vfranca3@gmail.com>
2021-08-29 00:48:39 -0300

    Atualizado arquivos de configuracao do terminal para um ativo

--- mtcli/s.bat
+++ mtcli/s.bat
@@ -1,19 +1,11 @@
 @echo off
-rem aliases para comandos do mtcli
-rem exibe/define o symbol do terminal
-set symbol=%1
-rem ajuda
-if "%1" == "-h" (
-echo exibe/define o symbol
-goto :EOF
-)
-rem comandos
-rem exibe o symbol
+rem mtcli
+rem variaveis de ambiente
 if "%1" == "" (
 echo symbol=%t%
 goto :EOF
 )
-rem define o symbol
-set t=%symbol%
-title %symbol%
+set t=%1
+set symbol=%1
+title %t%
 echo symbol=%t%
