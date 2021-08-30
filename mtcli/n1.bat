bce2fa1a558a4d417f283e2ee01fde287df60e1f
Valmir Franca <vfranca3@gmail.com>
2021-08-30 18:15:50 -0300

    Adicionadas notas nas variaveis de ambiente

---
+++ mtcli/n1.bat
@@ -0,0 +1,9 @@
+@echo off
+rem mtcli
+rem variáveis de ambiente
+rem nota 1
+if "%1" == "" (
+echo %n1%
+goto :EOF
+)
+set n1=%1
---
+++ mtcli/n4.bat
@@ -0,0 +1,9 @@
+@echo off
+rem mtcli
+rem variáveis de ambiente
+rem nota 4
+if "%1" == "" (
+echo %n4%
+goto :EOF
+)
+set n4=%1
