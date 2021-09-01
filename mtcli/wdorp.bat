6d31875b67a5b0f23b8884f003fddedfed55aa7a
Valmir Franca <vfranca3@gmail.com>
2021-09-01 04:54:18 -0300

    Adicionados atalhos para abertura de relatorios txt de ativos

---
+++ mtcli/wdorp.bat
@@ -0,0 +1,6 @@
+@echo off
+rem mtcli
+rem situação do ativo
+set arq=wdo.txt
+echo %arq%
+type %arq%
---
+++ mtcli/winrp.bat
@@ -0,0 +1,6 @@
+@echo off
+rem mtcli
+rem situação do ativo
+set arq=win.txt
+echo %arq%
+type %arq%
