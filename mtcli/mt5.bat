471abbb17b5ba7bd4c33e4972587d247d1177776
Valmir Franca <vfranca3@gmail.com>
2021-09-06 10:59:58 -0300

    Alterada mt5.bat paa ferenciar instalacoes mt5

--- mtcli/mt5.bat
+++ mtcli/mt5.bat
@@ -1,7 +1,16 @@
 @echo off
 rem mtcli
 rem Altera o MT5 do mtcli
-set envdir=C:\
+set envDir=C:\
 rem set mt5="%1"
 set /p mt5=corretora:
-copy %envdir%.env-%mt5% %envdir%.env
+set arquivoOrig="%envDir%.env-%mt5%"
+set arquivoDest="%envDir%.env"
+if exist %arquivoOrig% (
+copy %arquivoOrig% %arquivoDest%
+echo mt5 %mt5% ativado
+goto :EOF
+) else (
+echo corretora inexistente. Verifique o nome e tente novamente
+goto :EOF
+)
