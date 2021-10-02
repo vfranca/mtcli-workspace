a919f2f16fbb83c32af32d2d9adc4a21ebf4d62e
Valmir Franca <vfranca3@gmail.com>
2021-10-02 01:43:26 -0300

    Atualizada test.bat para backtest

--- mtcli/test.bat
+++ mtcli/test.bat
@@ -1,26 +1,24 @@
 @echo off
-rem Prepara o ambiente para backtest
-rem variaveis de entrada
-set s=%1
-set d=%2
-set m=%3
-set y=2020
-set p=%4
-set nomearquivo=%p%
-set tempdir=var
-rem ajuda
-if "%1" == "" (
-echo Prepara o terminal para backtest
-echo uso:
-echo "test <ativo> <dia> <mes> <periodo>"
+rem mtcli
+rem backtest 
+set /p s=ativo:
+set /p d=dia:
+set /p m=mes:
+set /p y=ano:
+set /p p=tempo grafico:
+set file=%p%
+set count=114
+set tempdir=%TEMP%
+rem **********
+if "%1" == "-h" (
+echo gera os arquivos dos graficos para backtest
 goto :EOF
 )
-rem comandos
-rem gera os arquivos dos graficos
-mt bars %s% --date %y%.%m%.%d% --period %p% --count 109 > %tempdir%\%nomearquivo%-default.txt
-mt bars %s% --date %y%.%m%.%d% --period %p% --view ch --count 109 > %tempdir%\%nomearquivo%-canal.txt
-mt bars %s% --date %y%.%m%.%d% --period %p% --view r --count 109 > %tempdir%\%nomearquivo%-ranges.txt
+rem *****
+mt bars %s% --date %y%.%m%.%d% --period %p% --count %count% > %tempdir%\%file%-default.txt
+mt bars %s% --date %y%.%m%.%d% --period %p% --view ch --count %count% > %tempdir%\%file%-canal.txt
+mt bars %s% --date %y%.%m%.%d% --period %p% --view r --count %count% > %tempdir%\%file%-ranges.txt
 rem abre os arquivos no bloco de notas
-start %tempdir%\%nomearquivo%-default.txt
-start %tempdir%\%nomearquivo%-canal.txt
-start %tempdir%\%nomearquivo%-ranges.txt
+start %tempdir%\%file%-default.txt
+start %tempdir%\%file%-canal.txt
+start %tempdir%\%file%-ranges.txt
