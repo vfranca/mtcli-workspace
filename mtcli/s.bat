93154773146b8f8a7761d756b7a02d07d0f49567
Valmir Franca <vfranca3@gmail.com>
2021-11-01 02:19:12 -0300

    Corrigido s.bat

--- mtcli/s.bat
+++ mtcli/s.bat
@@ -1,5 +1,6 @@
 @echo off
 rem mtcli
+rem Copyright 2021 Valmir França
 rem variaveis de ambiente
 if "%~1" == "" (
 echo SYMBOL=%t%
@@ -9,4 +10,5 @@ set t=%1
 set SYMBOL=%t%
 rem set id=%ano%.%mes%.%dia%
 title %t%
+prompt $$
 echo SYMBOL=%t%
