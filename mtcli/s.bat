c1d448dab1b2839fbadfd05c95465939e327c1ba
Valmir Franca <vfranca3@gmail.com>
2021-11-01 02:41:43 -0300

    Adicionada informacao do digito no s.bat

--- mtcli/s.bat
+++ mtcli/s.bat
@@ -2,8 +2,10 @@
 rem mtcli
 rem Copyright 2021 Valmir França
 rem variaveis de ambiente
+set envfile=%SystemDrive%/.env
 if "%~1" == "" (
 echo %t%
+dotenv --file %envfile% get DIGITS
 goto :EOF
 )
 set t=%1
