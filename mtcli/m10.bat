317cabd55d40ed039029db3f0eb7527be0db04d0
Valmir Franca <vfranca3@gmail.com>
2022-01-13 14:45:09 -0300

    Adicionados atalhos para M10

---
+++ mtcli/m10.bat
@@ -0,0 +1,7 @@
+@echo off
+rem aliases para comandos do mtcli
+rem grafico de máximas e mínimas do M10
+set p=M10
+call maxmin %*
+time /t
+
---
+++ mtcli/mm10.bat
@@ -0,0 +1,6 @@
+@echo off
+rem mtcli
+rem gráfico cheio do M10
+set p=M10
+call default %*
+time /t
