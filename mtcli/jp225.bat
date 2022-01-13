1025c42bb66b535c76464ea6a3bbcf10f043ad51
Valmir Franca <vfranca3@gmail.com>
2022-01-13 02:04:47 -0300

    Alterados configuradores de ativos da XM

--- mtcli/jp225.bat
+++ mtcli/jp225.bat
@@ -1,8 +1,9 @@
 @echo off
 rem mtcli
 rem Copyright 2021 Valmir França
-rem configuração de terminal
+rem configuração de ativo
 set prefix=JP225
-set sufix=Cash
+set /p sufix=contrato:
+set sufix=-%sufix%
 set digits=0
-call terminal
+call perfil
--- mtcli/us30.bat
+++ mtcli/us30.bat
@@ -1,8 +1,9 @@
 @echo off
 rem mtcli
 rem Copyright 2021 Valmir França
-rem configuração de terminal
+rem configuração de ativo
 set prefix=US30
-set sufix=Cash
+set /p sufix=contrato:
+set sufix=-%sufix%
 set digits=0
-call terminal
+call perfil
