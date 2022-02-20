916f369b5cae6825f14ec005da926c869094764b
Valmir Franca <vfranca3@gmail.com>
2022-02-19 23:02:18 -0300

    Reformulado o deploy

--- deploy2.bat
+++ deploy2.bat
@@ -1,5 +1,5 @@
 @echo off
-set fzip=mtcli_2.00.zip
-set dir=mtcli_2.00
+set fzip=mtcli-deprecated.zip
+set dir=mtcli-deprecated
 call rar a %fzip% %dir%
 move %fzip% "%HOMEPATH%\Google Drive\%fzip%"
