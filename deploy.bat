df7f8389f419bf8ee62da2bbde60a987b8ccae55
Valmir Franca <vfranca3@gmail.com>
2022-03-07 07:35:36 -0300

    Adicionados novos destinos ao deploy

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,12 @@
 @echo off
 set fzip=mtcli.zip
 set dir=mtcli
+set dirdest1="G:\Meu Drive"
+set dirdest2=%HOMEPATH%
+set dirdest3=%SYSTEMDRIVE%
 call rar a %fzip% %dir%
-move %fzip% "%HOMEPATH%\Google Drive\%fzip%"
+copy %fzip% %dirdest1%\%fzip%
+copy %fzip% %dirdest2%\%fzip%
+copy %fzip% %dirdest3%\%fzip%
+del %fzip%
+
