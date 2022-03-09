c6d84de219594e32d261392e5f0297bb529de4de
Valmir Franca <vfranca3@gmail.com>
2022-03-09 08:15:41 -0300

    Reformulado alteracao do symbol

--- mtcli/s.bat
+++ mtcli/s.bat
@@ -10,6 +10,7 @@ goto :EOF
 set SYMBOL=%1
 set s=%SYMBOL%
 dotenv --file %envfile% set SYMBOL %1
+if "%2" neq "" call d %2
 
 if not defined id set id=""
 if not defined intraday set intraday=desligado
