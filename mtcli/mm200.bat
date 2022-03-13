1f866a47d2590f597812c146971e77d4650fea04
Valmir Franca <vfranca3@gmail.com>
2022-03-13 18:27:36 -0300

    Refatorado mm200

--- mtcli/mm200.bat
+++ mtcli/mm200.bat
@@ -4,5 +4,11 @@ rem Copyright 2021 Valmir França
 rem atalhos de comando
 rem médias móveis de 20 e 200 períodos
 mt bars %SYMBOL% --period d1 --view c --count 1
-mt mm %SYMBOL% --period d1 --count 20
-mt mm %SYMBOL% --period d1 --count 200
+set count=20
+call :media
+set count=200
+call :media
+goto :EOF
+
+:media
+mt mm %SYMBOL% --period d1 --count %count%
