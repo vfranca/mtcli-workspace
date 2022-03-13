5aed83231741179bfae018ea1dd1f39e9561f207
Valmir Franca <vfranca3@gmail.com>
2022-03-13 06:25:01 -0300

    Reformulado atalho para volume de negocios n.bat

--- mtcli/n.bat
+++ mtcli/n.bat
@@ -1,4 +1,11 @@
 @echo off
 rem mtcli
-rem volume de negócios
-mt bars %t% -p daily -c 1 -v vol
+rem Copyright 2021 Valmir França
+rem atalhos de comando
+rem volume de negócios da barra corrente
+if "%1" == "" (
+set period1=%PERIOD%
+) else (
+set period1=%1
+)
+mt bars %SYMBOL% --count 1 --view vol --period %period1%
