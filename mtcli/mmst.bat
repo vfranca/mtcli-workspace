34dd4c5ff5cd01d5bf86df55ff692bbe13c5c0b3
Valmir Franca <vfranca3@gmail.com>
2022-03-13 18:41:57 -0300

    Refatorado mmst.bat

--- mtcli/mmst.bat
+++ mtcli/mmst.bat
@@ -1,9 +1,20 @@
 @echo off
+rem mtcli
+rem Copyright 2021 Valmir França
+rem atalhos de comando
 if "%~1" == "" (
-set s=%SYMBOL%
+set symbol1=%SYMBOL%
 ) else (
-set s=%1
+set symbol1=%1
 )
-mt bars %s% --period D1 --view c --count 1
-mt mm %s% --period D1 --count 20
-mt mm %s% --period D1 --count 100
+
+mt bars %symbol1% --period D1 --view c --count 1
+
+set count=20
+call :media
+set count=100
+call :media
+goto :EOF
+
+:media
+mt mm %symbol1% --period D1 --count %count%
