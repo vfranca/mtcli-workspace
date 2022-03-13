a21654235458edf23f8f51addc70e0c9f9b4b8fb
Valmir Franca <vfranca3@gmail.com>
2022-03-13 18:23:38 -0300

    Refatorado o atalho mm9.bat

--- mtcli/mm9.bat
+++ mtcli/mm9.bat
@@ -4,5 +4,11 @@ rem Copyright 2021 Valmir França
 rem atalhos de comando
 rem médias móveis de 9 e de 20 períodos do diário
 mt bars %SYMBOL% --period D1 --view c --count 1
-mt mm %SYMBOL% --period D1 --count 9
-mt mm %SYMBOL% --period D1 --count 20
+set count=9
+call :media
+set count=20
+call :media
+goto :EOF
+
+:media
+mt mm %SYMBOL% --period D1 --count %count%
