b066ccba05ec3adce562d0a5ed1a24d1d3436373
Valmir Franca <vfranca3@gmail.com>
2022-03-13 06:11:51 -0300

    Reformulada range da barra corrente atalho g

--- mtcli/g.bat
+++ mtcli/g.bat
@@ -2,5 +2,10 @@
 rem mtcli
 rem Copyright 2021 Valmir França
 rem atalhos de comando
-rem range do dia
-mt bars %SYMBOL% --view r --period D1 --count 1
+rem range da barra corrente
+if "%1" == "" (
+set period1=%PERIOD%
+) else (
+set period1=%1
+)
+mt rm %SYMBOL% --period %period1% --count 1
