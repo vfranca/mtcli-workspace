50c0ebb72c94246255eb8511d34894c0747a031a
Valmir Franca <vfranca3@gmail.com>
2022-03-14 08:24:25 -0300

    Refatorado mmdt medias do day trade

--- mtcli/mmdt.bat
+++ mtcli/mmdt.bat
@@ -4,5 +4,12 @@ rem Copyright 2021 Valmir França
 rem atalhos de comando
 rem médias móveis do day trade
 mt bars %SYMBOL% --view c --count 1 --period %PERIOD%
-mt mm %SYMBOL% --period M5 --count 20
-mt mm %SYMBOL% --period M5 --count 240
+
+set count=20
+call :media
+set count=200
+call :media
+goto :EOF
+
+:media
+mt mm %SYMBOL% --period M6 --count %count%
