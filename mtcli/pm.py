365c06ce4e37b5d9fcc6c13c47195384756cf3c7
Valmir Franca <vfranca3@gmail.com>
2022-03-18 19:46:28 -0300

    Alterado pm.py para calculo do preco medio

--- mtcli/pm.py
+++ mtcli/pm.py
@@ -1,12 +1,19 @@
+"""
+mtcli
+Copyright 2021 Valmir França
+ferramentas de trading
+calcula o preço médio
+"""
 import click
 
 
 @click.command()
-@click.argument("p1", type=float)
-@click.argument("p2", type=float)
-def cli(p1, p2):
-	pm = (p1 + p2) / 2
-	click.echo("%.2f" % pm)
+@click.argument("preco1", type=float)
+@click.argument("preco2", type=float)
+def cli(preco1, preco2):
+	""" Calcula o preço médio"""
+	medio = (preco1 + preco2) / 2
+	click.echo("%.2f" % medio)
 
 
 if __name__ == "__main__":
