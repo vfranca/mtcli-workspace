b0f85ab404512237e6f1d2c3fd1db51bc4eafa68
Valmir Franca <vfranca3@gmail.com>
2022-03-20 09:08:17 -0300

    Removidos arquivos obsoletos

--- mtcli/m20r.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem mtcli
-rem Copyright 2021 Valmir França
-set PERIOD=M20
-call ranges %*
-time /t
--- mtcli/omn1.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem mtcli
-rem atalhos mtcli
-rem oscilação percentual do mensal
-mt bars %t% -c 1 -v var -p monthly
