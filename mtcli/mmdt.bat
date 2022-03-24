3fa046c36e528985b50cb8be543371162ce49fa1
Valmir Franca <vfranca3@gmail.com>
2022-03-24 07:23:30 -0300

    Adicionadas medias do m3, m4, m6, m15 e h1

--- mtcli/mmdt.bat
+++ mtcli/mmdt.bat
@@ -4,15 +4,11 @@ rem Copyright 2021 Valmir França
 rem atalhos de comando
 rem médias móveis do day trade
 mt bars %SYMBOL% --view c --count 1 --period %PERIOD%
-rem M5
-if "PERIOD" == "M5" (
-set medias=(20, 240)
-)
-rem M10
-if "PERIOD" == "M10" (
-set medias=(20, 120)
-)
-:media
-for %%i in %medias% do (
-mt mm %SYMBOL% --period %PERIOD% --count %%i
-)
+if %PERIOD% == M3 set medias=(20, 400)
+if %PERIOD% == M4 set medias=(20, 300)
+if %PERIOD% == M5 set medias=(20, 240)
+if %PERIOD% == M6 set medias=(20, 200)
+if %PERIOD% == M10 set medias=(20, 120)
+if %PERIOD% == M15 set medias=(20, 80)
+if %PERIOD% == H1 set medias=(20, 80)
+for %%i in %medias% do mt mm %SYMBOL% --period %PERIOD% --count %%i
