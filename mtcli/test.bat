d914492d5c59fcafa0b7c256e9784bb5acfc154c
Valmir Franca <vfranca3@gmail.com>
2022-04-25 02:16:20 -0300

    Alterado test.bat para backtest

--- mtcli/test.bat
+++ mtcli/test.bat
@@ -12,15 +12,10 @@ set file=%p%
 set count=180
 set tempdir=%TEMP%
 rem **********
-if "%1" == "-h" (
-echo gera os arquivos dos graficos para backtest
-goto :EOF
-)
-rem *****
-mt bars %s% --date %y%.%m%.%d% --period %p% --count %count% > %tempdir%\%file%-default.txt
+mt bars %s% --date %y%.%m%.%d% --period %p% --count %count% > %tempdir%\%file%-completo.txt
 mt bars %s% --date %y%.%m%.%d% --period %p% --view ch --count %count% > %tempdir%\%file%-canal.txt
-mt bars %s% --date %y%.%m%.%d% --period %p% --view r --count %count% > %tempdir%\%file%-ranges.txt
+rem mt bars %s% --date %y%.%m%.%d% --period %p% --view r --count %count% > %tempdir%\%file%-ranges.txt
 rem abre os arquivos no bloco de notas
-start %tempdir%\%file%-default.txt
 start %tempdir%\%file%-canal.txt
-start %tempdir%\%file%-ranges.txt
+start %tempdir%\%file%-completo.txt
+rem start %tempdir%\%file%-ranges.txt
