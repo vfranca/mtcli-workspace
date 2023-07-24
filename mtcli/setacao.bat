c46ef5b636bf1a453f48ac3ac9893a39a15de1a7
Valmir Franca <vfranca3@gmail.com>
2023-07-24 02:32:17 -0300

    Adicionados arquivos de configuracao de perfil do terminal

---
+++ mtcli/setacao.bat
@@ -0,0 +1,6 @@
+@echo off
+call lot 200
+call ala 125
+call rr 3
+call tam 1
+call moeda BRL
---
+++ mtcli/setpcar.bat
@@ -0,0 +1,7 @@
+@echo off
+call setdt
+call setacao
+call s PCAR3 2
+call r 0.15
+call coy 21.00
+call rdia 1.15
