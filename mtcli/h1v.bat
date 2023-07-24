2ebfc6dbd83f7e5128271ef0fe1d7e2ed83b6fcd
Valmir Franca <vfranca3@gmail.com>
2023-07-24 02:31:22 -0300

    Adicionados atalhos para graficos e volume no intraday

---
+++ mtcli/h1v.bat
@@ -0,0 +1,7 @@
+@echo off
+rem Copyright 2023 Valmir França da Silva
+rem https://github.com/vfranca
+rem
+set PERIOD=H1
+set VIEW=vol
+call barras %*
---
+++ mtcli/m2v.bat
@@ -0,0 +1,7 @@
+@echo off
+rem Copyright 2023 Valmir França da Silva
+rem https://github.com/vfranca
+rem
+set PERIOD=M2
+set VIEW=vol
+call barras %*
