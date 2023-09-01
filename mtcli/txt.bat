1d93da5e43ff7686790a93c08ff480099527a6db
Valmir Franca <vfranca3@gmail.com>
2023-09-01 02:48:04 -0300

    Modificado o atalho para abrir graficos do intraday no bloco de notas

--- mtcli/txt.bat
+++ mtcli/txt.bat
@@ -8,80 +8,46 @@ set volumes=0
 set completo=1
 set count=678
 set count_swing=20
-rem
-rem abre um grafico minimo do periodo atual
-rem
-set periodos=(mn1,w1,d1,h4,h3,h2,h1,m30,m20,m15,m12,m10,m6,m5,m4,m3,m2,m1)
-for %%i in %periodos% do (
-if "%1" == "%%i" (
-call dt 0
-call %%i %2 > %%i.txt
-start %%i.txt
-goto :EOF
-)
-)
-rem
 rem define o ativo
-rem
-:symbol
 if not defined S (set /p s=ativo:)
-rem
 rem define o período
-:period
 if not defined P (set /p p=periodo:)
-rem
 rem define o ano
-:ano
 if not "%P%" == "MN1" (
 if not "%P%" == "W1" (
 if not "%P%" == "D1" (
 if not defined Y (set /p y=ano:)
-)
-)
-)
-rem
+)))
 rem define o mês
-:mes
 if not "%P%" == "MN1" (
 if not "%P%" == "W1" (
 if not "%P%" == "D1" (
 if not defined M (set /p m=mes:)
 )))
-rem
 rem define o dia
-:dia
 if not "%P%" == "MN1" (
 if not "%P%" == "W1" (
 if not "%P%" == "D1" (
 if not defined I (set /p i=dia:)
 )))
-rem
-rem define o sufixo do nome de arquivo
-:sufixo
-set sufix=%P%
-rem
+rem define o prefixo do nome do arquivo txt
+set prefixo=%S%-%P%
+rem define o sufixo do nome do arquivo txt
+set sufixo=%I%-%M%-%Y%.txt
 rem define quantidade de barras
-:barras
 if not "%~1" == "" (
 set count=%1
 set count_swing=%1
 )
-rem
 rem define o diretório dos arquivos
-:diretorio
-set tempdir=%TEMP%
-rem 
+set dir_txt=%TEMP%
 rem define o nome dos arquivos
-rem
-:nomeiatxt
-rem gráfico de maximas e minimas
-set filename=%tempdir%\%sufix%.txt
+rem gráfico minimo
+set arquivo_txt=%dir_txt%\%prefixo%-%sufixo%
 rem gráfico de ranges
-set filename-ranges=%tempdir%\ranges-%sufix%.txt
-rem gráfico de volumes
-set filename-volumes=%tempdir%\volumes-%sufix%.txt
+set arquivo_txt-ranges=%dir_txt%\%prefixo%-ranges-%sufixo%
 rem gráfico completo
-set filename-full=%tempdir%\full-%sufix%.txt
+set arquivo_txt-full=%dir_txt%\%prefixo%-full-%sufixo%
 rem
 rem gera os arquivos
 :geratxt
@@ -92,61 +58,49 @@ goto :DAYTRADE
 rem
 rem day trade
 :daytrade
-rem gráfico de máximas e mínimas
+rem gráfico mínimo
 if %minimo% == 1 (
-mt bars %S% --date %Y%.%M%.%I% --period %P% --view ch --count %count% > %filename%
+mt bars %S% --date %Y%.%M%.%I% --period %P% --view ch --count %count% > %arquivo_txt%
 )
 rem gráfico de ranges
 if %ranges% == 1 (
-mt bars %S% --date %Y%.%M%.%I% --period %P% --view r --count %count% > %filename-ranges%
-)
-rem gráfico de volumes
-if %volumes% == 1 (
-mt bars %S% --date %Y%.%M%.%I% --period %P% --view vol --count %count% > %filename-volumes%
+mt bars %S% --date %Y%.%M%.%I% --period %P% --view r --count %count% > %arquivo_txt-ranges%
 )
 rem gráfico completo
 if %completo% == 1 (
-mt bars %S% --date %Y%.%M%.%I% --period %P% --count %count% > %filename-full%
+mt bars %S% --date %Y%.%M%.%I% --period %P% --count %count% > %arquivo_txt-full%
 )
 goto :ABRETXT
 rem
 rem swing trade
 :swingtrade
-rem gráfico de máximas e mínimas
+rem gráfico mínimo
 if %minimo% == 1 (
-mt bars %S% --period %P% --view ch --count %count_swing% > %filename%
+mt bars %S% --period %P% --view ch --count %count_swing% > %arquivo_txt%
 )
 rem gráfico de ranges
 if %ranges% == 1 (
-mt bars %S% --period %P% --view r --count %count_swing% > %filename-ranges%
-)
-rem gráfico de volumes
-if %volumes% == 1 (
-mt bars %S% --period %P% --view vol --count %count_swing% > %filename-volumes%
+mt bars %S% --period %P% --view r --count %count_swing% > %arquivo_txt-ranges%
 )
 rem gráfico completo
 if %completo% == 1 (
-mt bars %S% --period %P% --count %count_swing% > %filename-full%
+mt bars %S% --period %P% --count %count_swing% > %arquivo_txt-full%
 )
 goto :ABRETXT
 rem
 rem abre os arquivos no bloco de notas
 :ABRETXT
-rem abre o gráfico de máximas e mínimas
+rem abre o gráfico mínimo
 if %minimo% == 1 (
-start %filename%
+start %arquivo_txt%
 )
 rem abre o gráfico de ranges
 if %ranges% == 1 (
-start %filename-ranges%
-)
-rem abre o gráfico de volumes
-if %volumes% == 1 (
-start %filename-volumes%
+start %arquivo_txt-ranges%
 )
 rem abre o gráfico completo
 if %completo% == 1 (
-start %filename-full%
+start %arquivo_txt-full%
 )
 goto :EOF
 rem
