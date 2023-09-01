f11f8623730a30be25bd08d57256fb1782c6a445
Valmir Franca <vfranca3@gmail.com>
2023-09-01 02:47:15 -0300

    Modificado atalho para configurar o terminal para win

--- mtcli/setwin.bat
+++ mtcli/setwin.bat
@@ -1,11 +1,11 @@
 @echo off
 call setdt
-call s WINQ23 0
+call s WINV23 0
 call lot 2
 call ala 240
-call r 150
+call r 200
 call rr 3
 call tam 0.2
-call coy 119000
-call rdia 2000
+call coy 120000
+call rdia 1900
 call moeda BRL
