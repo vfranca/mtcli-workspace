822d68fcaacbb8b191d50948d23df3eda015ccb8
Valmir Franca <vfranca3@gmail.com>
2023-12-02 07:35:58 -0300

    Adicionada opcao default ao exp.bat

--- mtcli/exp.bat
+++ mtcli/exp.bat
@@ -2,7 +2,7 @@
 rem pasta mtcli
 rem Copyright 2021-2023 Valmir França da Silva
 rem https://github.com/vfranca/
-rem exporta um grafico para um arquivo TXT
+rem exporta o grafico para um arquivo TXT
 rem define o nome do arquivo de exportacao
 set arquivo_exp=%SYMBOL%-%1-%I%-%M%-%Y%.txt
 rem define a pasta dos arquivos
@@ -12,6 +12,10 @@ if not exist %pasta_exp% (
 mkdir %pasta_exp%
 )
 rem gera o arquivo de exportacao
+if "%1" == "" (
+call %PERIOD% 20 > %pasta_exp%\%arquivo_exp%
+) else (
 call %* > %pasta_exp%\%arquivo_exp%
+)
 rem abre o arquivo de exportacao
 start %pasta_exp%\%arquivo_exp%
