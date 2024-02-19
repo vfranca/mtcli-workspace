56baab26fb4cceafd8ddfae880c08b5ccbc0c467
Valmir França <vfranca3@gmail.com>
2024-02-19 08:37:08 -0300

    retirado o fechamento de ma.bat

--- mtcli/ma.bat
+++ mtcli/ma.bat
@@ -3,15 +3,10 @@ rem pasta mtcli
 rem Copyright 2021-2023 Valmir França da Silva
 rem https://github.com/vfranca/
 rem media movel
-rem exibe o preço de fechamento
-mt bars %SYMBOL% --period %PERIOD% --view c --count 1
-rem
 rem Exibe uma média de n barras
-rem
 if not "%1" == "" (
 mt ma %SYMBOL% --period %PERIOD% --count %1
 goto :EOF
 )
-rem
 rem exibe a ema20
 mt ma %SYMBOL% --period %PERIOD% --count 20
