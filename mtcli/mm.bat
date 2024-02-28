90d49ffecc4ad2fcd64d21b39d5a844dcec0abe7
Valmir França <vfranca3@gmail.com>
2024-02-28 08:38:44 -0300

    removido o fechamento de mm.bat

--- mtcli/mm.bat
+++ mtcli/mm.bat
@@ -3,7 +3,7 @@ rem pasta mtcli
 rem Copyright 2021-2023 Valmir França da Silva
 rem https://github.com/vfranca/
 rem média móvel
-mt bars %SYMBOL% --period %PERIOD% --view c --count 1
+rem mt bars %SYMBOL% --period %PERIOD% --view c --count 1
 if "%1" == "" (
 set count=20
 goto :media
