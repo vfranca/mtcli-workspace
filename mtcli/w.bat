9e03ca88bb1cacf1552b0da21af1b39cf901ca4d
Valmir França <vfranca3@gmail.com>
2024-03-06 01:11:24 -0300

    revisado w.bat

--- mtcli/w.bat
+++ mtcli/w.bat
@@ -2,12 +2,9 @@
 rem mtcli
 rem Copyright 2022 Valmir França
 rem variaveis de ambiente
-set envfile=%SystemDrive%/.env
 if "%1" == "" (
-rem dotenv --file %envfile% get VIEW
 echo %VIEW%
 goto :EOF
 )
 set VIEW=%1
 set w=%VIEW%
-rem dotenv --file %envfile% set VIEW %1
