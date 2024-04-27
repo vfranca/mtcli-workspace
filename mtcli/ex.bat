67a93f5158cb185f245718bc653e2a9a51fb3b6d
Valmir França <vfranca3@gmail.com>
2024-04-27 04:45:57 -0300

    alteradas pastas de destino dos graficos exportados

--- mtcli/ex.bat
+++ mtcli/ex.bat
@@ -1,20 +1,26 @@
 @echo off
-rem pasta mtcli
-rem Copyright 2021-2023 Valmir França da Silva
-rem https://github.com/vfranca/
+rem mtcli
 rem exporta o grafico para um arquivo TXT
+rem
+rem
+rem define a pasta dos arquivos
+set pasta_exp=GRAFICOS\EX\%SYMBOL%
+rem
+rem
 rem define o nome do arquivo de exportacao
 if "%1" == "" (
 set arquivo_exp=%SYMBOL%-%PERIOD%-%Y%-%M%-%I%.txt
 ) else (
 set arquivo_exp=%SYMBOL%-%1-%Y%-%M%-%I%.txt
 )
-rem define a pasta dos arquivos
-set pasta_exp=ex\%SYMBOL%
+rem
+rem
 rem cria a pasta de exportacao
 if not exist %pasta_exp% (
 mkdir %pasta_exp%
 )
+rem
+rem
 rem gera o arquivo de exportacao
 if "%1" == "" (
 call %PERIOD% 20 > %pasta_exp%\%arquivo_exp%
--- mtcli/tx.bat
+++ mtcli/tx.bat
@@ -1,13 +1,21 @@
 	@echo off
-rem pasta mtcli
-rem Copyright 2021-2023 Valmir França da Silva
-rem https://github.com/vfranca/
+rem mtcli
 rem abre no bloco de notas graficos intradiários
+rem
 rem imputs
+rem
+rem diretório dos arquivos
+set dir_txt=GRAFICOS\TX\%SYMBOL%
+rem
+rem
 rem ativo
 if not defined S (set /p s=ativo:)
+rem
+rem
 rem período
 if not defined P (set /p p=periodo:)
+rem
+rem
 rem ano, mês e dia
 if not "%P%" == "MN1" (
 if not "%P%" == "W1" (
@@ -16,18 +24,24 @@ if not defined Y (set /p y=ano:)
 if not defined M (set /p m=mes:)
 if not defined I (set /p i=dia:)
 )))
+rem
+rem
 rem quantidade de barras
 set count=678
-rem diretório dos arquivos
-set dir_txt=tx\%SYMBOL%
+rem
+rem
+rem
 rem nomes dos arquivos
-set txt_min=%P%-%S%-%M%-%I%.txt
-set txt_ranges=%P%R-%S%-%M%-%I%.txt
-set txt_full=M%P%-%S%-%M%-%I%.txt
+set txt_min=%S%-%P%-%M%-%I%.txt
+set txt_ranges=%S%-%P%R-%M%-%I%.txt
+set txt_full=%S%-%P%F-%M%-%I%.txt
+rem
+rem
 rem cria a pasta dos arquivos
 if not exist %dir_txt% (
 mkdir %dir_txt%
 )
+rem
 rem exporta os arquivos
 rem gráfico cheio
 if "%1" == "f" (
