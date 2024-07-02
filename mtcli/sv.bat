7efee5a6f2ced3843e648c11323a8acf2c39a814
Valmir França <vfranca3@gmail.com>
2024-07-01 21:44:47 -0300

    Corrigido espacamento no sv.bat

--- mtcli/sv.bat
+++ mtcli/sv.bat
@@ -1,34 +1,33 @@
 @echo off
 set fname=%SYMBOL%.bat
 echo @echo off>%fname%
-echo set coy=%COY%>>%fname%
-echo set ood=%ood%>>%fname%
-echo set hoy=%hoy%>>%fname%
-echo set loy=%loy%>>%fname%
+echo set coy=%COY% >>%fname%
+echo set ood=%OOD% >>%fname%
+echo set hoy=%HOY% >>%fname%
+echo set loy=%LOY% >>%fname%
 
 echo set how=%HOW% >>%fname%
 echo set low=%LOW% >>%fname%
 echo set cow=%COW% >>%fname%
 
-echo set h=%h%>>%fname%
-echo set l=%l%>>%fname%
-echo set t1=%t1%>>%fname%
-echo set t2=%t2%>>%fname%
-echo set f1=%f1%>>%fname%
-echo set f2=%f2%>>%fname%
-echo set tl=%tl%>>%fname%
-echo set tcl=%tcl%>>%fname%
-echo set tg=%TG%>>%fname%
-echo set sl=%SL%>>%fname%
-echo set e=%E%>>%fname%
-echo set tga=%TGA%>>%fname%
-echo set tgb=%TGB%>>%fname%
-echo set sld=%SLD%>>%fname%
-echo set mta=%MTA%>>%fname%
-echo set lmt=%LMT%>>%fname%
+echo set h=%H% >>%fname%
+echo set l=%L% >>%fname%
+echo set t1=%T1% >>%fname%
+echo set t2=%T2% >>%fname%
+echo set f1=%F1% >>%fname%
+echo set f2=%F2% >>%fname%
+echo set tl=%TL% >>%fname%
+echo set tcl=%TCL% >>%fname%
+echo set tg=%TG% >>%fname%
+echo set sl=%SL% >>%fname%
+echo set e=%E% >>%fname%
+echo set tga=%TGA% >>%fname%
+echo set tgb=%TGB% >>%fname%
+echo set sld=%SLD% >>%fname%
+echo set mta=%MTA% >>%fname%
+echo set lmt=%LMT% >>%fname%
 echo set r=%R% >>%fname%
 echo set rr=%RR% >>%fname%
-echo set e=%E%>>%fname%
 echo set lot=%LOT% >>%fname%
 echo set tam=%TAM% >>%fname%
 echo set ala=%ALA% >>%fname%
