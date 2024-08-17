887d496aa890cd6f2014ad7cc031d8e5a335451c
Valmir França <vfranca3@gmail.com>
2024-08-17 12:55:11 -0300

    alterados arquivos de configuracao

--- mtcli/conf/cger40.bat
+++ mtcli/conf/cger40.bat
@@ -5,7 +5,8 @@ call p M5
 call ala 200
 call tam 1
 call lot 0.25
-call r 20
+call r 16
 call rr 3
 call coy 18600
 call moeda EUR
+call spread 1.9
--- mtcli/conf/cusa500.bat
+++ mtcli/conf/cusa500.bat
@@ -1,6 +1,7 @@
 @echo off
 call confdt
 call s USA500 0
+call p M5
 call ala 200
 call tam  1
 call lot 0.5
@@ -8,3 +9,4 @@ call r 8
 call rr 3
 call coy 5600
 call moeda USD
+call spread 0.8
