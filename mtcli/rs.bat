682ee77698847c1e68798ba88dfd2a3bc1eba16f
Valmir França <vfranca3@gmail.com>
2024-08-17 12:46:58 -0300

    alterado rs.bat para reset de configuracoes

--- mtcli/rs.bat
+++ mtcli/rs.bat
@@ -1,7 +1,29 @@
 @echo off
-call s WINJ23
-call d 0
-call p M5
-call y %Y%
-call m %M%
-call i %I%
+set coy=
+set ood=
+set hoy=
+set loy=
+set h=
+set l=
+set t1=
+set t2=
+set f1=
+set f2=
+set tl=
+set tcl=
+set tg=
+set tga=
+set tgb=
+set sl=
+set e=
+set r=
+set rr=
+set tam=
+set ala=
+set sld=
+set lmt=
+set mta=
+call pm 0
+
+
+
