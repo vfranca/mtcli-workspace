2ef22781e707acbc76920862cbfa542bc9808261
Valmir França <vfranca3@gmail.com>
2024-09-10 01:19:52 -0300

    Redefinidos atralhos mt bars: h2, h3 e h4

--- mtcli/h2.bat
+++ mtcli/h2.bat
@@ -1,7 +1,7 @@
 @echo off
-rem pasta mtcli
-rem Copyright 2021-2023 Valmir França da Silva
-rem https://github.com/vfranca/
-set PERIOD=H2
-set VIEW=ch
-call barras %*
+rem mtcli
+rem gráfico
+call mtcli
+call p H2
+call w ch
+call bar %*
--- mtcli/h3r.bat
+++ mtcli/h3r.bat
@@ -1,7 +1,7 @@
 @echo off
-rem pasta mtcli
-rem Copyright 2021-2023 Valmir França da Silva
-rem https://github.com/vfranca/
-set PERIOD=H3
-set VIEW=r
-call barras %*
+rem mtcli
+rem gráfico
+call mtcli
+call p H3
+call w r
+call bar %*
