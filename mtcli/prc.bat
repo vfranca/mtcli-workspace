90ca7eca0c5e365e6cf1bc179f86d1f3cda1591d
Valmir França <vfranca3@gmail.com>
2024-09-12 06:18:02 -0300

    Adicionado exibicao da configuracao de price action prc.bat

---
+++ mtcli/prc.bat
@@ -0,0 +1,15 @@
+@echo off
+rem mtcli
+rem configurações de price action
+call mtcli
+echo MAXIMA %H%
+echo MINIMA %L%
+echo MOVIMENTO PROJETADO %MP%
+echo ALVO ABAIXO %TGB%
+echo ALVO ACIMA %TGA%
+echo LINHA DE TENDENCIA %TL%
+echo LINHA DE CANAL %TCL%
+echo MAXIMA DE ONTEM %HOY%
+echo MINIMA DE ONTEM %LOY%
+echo FECHAMENTO %COY%
+echo ABERTURA %OOD%
