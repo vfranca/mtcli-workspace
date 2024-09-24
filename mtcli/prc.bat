e22383d51f2f1c11058e4e2e20982cada07afbe1
Valmir França <vfranca3@gmail.com>
2024-09-24 04:57:09 -0300

    Atualizado prc.bat

--- mtcli/prc.bat
+++ mtcli/prc.bat
@@ -3,14 +3,13 @@ rem mtcli
 rem configurações de price action
 call mtcli
 echo CODIGO=%SYMBOL%
-echo MAXIMA=%H%
-echo MINIMA=%L%
-echo MOVIMENTO PROJETADO=%MP%
-echo ALVO ABAIXO=%TGB%
-echo ALVO ACIMA=%TGA%
-echo LINHA DE TENDENCIA=%TL%
-echo LINHA DE CANAL=%TCL%
 echo MAXIMA DE ONTEM=%HOY%
 echo MINIMA DE ONTEM=%LOY%
 echo FECHAMENTO=%COY%
 echo ABERTURA=%OOD%
+echo MAXIMA=%H%
+echo MINIMA=%L%
+echo MOVIMENTO PROJETADO ACIMA=%MMU%
+echo MOVIMENTO PROJETADO ABAIXO=%MMD%
+echo LINHA DE TENDENCIA=%TL%
+echo LINHA DE CANAL=%TCL%
