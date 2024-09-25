f4def4fc1c9df8a380dd7971499786667a756be2
Valmir França <vfranca3@gmail.com>
2024-09-25 03:22:23 -0300

    Adicionadas marcacoes gxh e gxl

---
+++ mtcli/gxh.bat
@@ -0,0 +1,9 @@
+@echo off
+rem mtcli
+rem marcações
+rem globex high
+if "%1" == "" (
+echo %GXH%
+goto :EOF
+)
+set gxh=%1
---
+++ mtcli/gxl.bat
@@ -0,0 +1,9 @@
+@echo off
+rem mtcli
+rem marcações
+rem globex low
+if "%1" == "" (
+echo %GXL%
+goto :EOF
+)
+set gxl=%1
