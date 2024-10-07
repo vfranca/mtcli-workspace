d988e7f1e9428327cc22b2f4b92f0164a0748b61
Valmir França <vfranca3@gmail.com>
2024-10-07 05:31:32 -0300

    Adicionados novos templates

---
+++ mtcli/TEMPLATES/teuro50.bat
@@ -0,0 +1,11 @@
+@echo off
+call s EURO50 0
+call p M5
+call ala 200
+call tam 1
+call lot 0.1
+call r 7
+call rr 3
+call coy 4940
+call moeda EUR
+call spread 1.9
---
+++ mtcli/TEMPLATES/tusarus.bat
@@ -0,0 +1,11 @@
+@echo off
+call s USARUS 0
+call p M5
+call ala 200
+call tam  1
+call lot 0.5
+call r 4
+call rr 3
+call coy 2200
+call moeda USD
+call spread 0.8
