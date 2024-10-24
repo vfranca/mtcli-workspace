c67c36cffab9f0679b067c652a79786f751f5bc8
Valmir França <vfranca3@gmail.com>
2024-10-24 08:04:51 -0300

    Reescrito script para calcular expectativa matematica

--- mtcli++/mtcli/BIN/em.py
+++ mtcli++/mtcli/BIN/em.py
@@ -7,8 +7,11 @@ import click
 @click.argument("prejuizo", type=float)
 @click.option("--trades", "-t", type=int, default=10)
 def em(taxa_acerto, lucro, prejuizo, trades):
-    expec_mat = (taxa_acerto/100 * lucro) - ((100 - taxa_acerto)/100 * prejuizo)
-    click.echo("%.2f" % expec_mat)
+    lucro_medio = (taxa_acerto / 100 * lucro) - ((100 - taxa_acerto) / 100 * prejuizo)
+    lucro_total = lucro_medio * trades
+    click.echo("lucro medio = %.2f" % lucro_medio)
+    click.echo("lucro total = %.2f" % lucro_total)
+    click.echo("trades = %i" % trades)
 
 
 if __name__ == "__main__":
--- mtcli++/mtcli/em.bat
+++ mtcli++/mtcli/em.bat
@@ -1,6 +1,5 @@
 @echo off
-rem pasta mtcli
-rem Copyright 2021-2023 Valmir França da Silva
-rem https://github.com/vfranca/
-rem Calcula a expectativa matemática
-gr em %*
+rem mtcli
+rem Calcula expectativa matematica
+call mtcli
+call %DIR_BIN%\em %*
