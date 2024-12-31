3d705f7f16c1de4c2a2cef1ceca30ab6f4ca2e4c
Valmir França <vfranca3@gmail.com>
2024-12-31 03:32:39 -0300

    adicionadas expansoes em pb.py

--- mtcliplus/mtcli/BIN/pb.py
+++ mtcliplus/mtcli/BIN/pb.py
@@ -14,13 +14,31 @@ digits = os.getenv("DIGITOS")
 def lg(begin, end):
     """Calcula range e pullbacks da perna"""
     leg = end - begin
-    pullback1 = end - leg * 1 / 3
-    pullback2 = end - leg * 1 / 2
-    pullback3 = end - leg * 2 / 3
+    pullback1 = end - leg * 0.33
+    pullback2 = end - leg * 0.5
+    pullback3 = end - leg * 0.66
+
     click.echo("%.{0}f".format(digits) % abs(leg))
+
+    click.echo("recuos")
     click.echo("%.{0}f".format(digits) % pullback1)
     click.echo("%.{0}f".format(digits) % pullback2)
     click.echo("%.{0}f".format(digits) % pullback3)
+
+    expansao1 = end + leg *0.33
+    expansao2 = end + leg * 0.50
+    expansao3 = end + leg *0.66
+    expansao4 = end + leg * 1
+
+    click.echo("expansoes")
+    click.echo("%.{0}f".format(digits) % expansao1)
+    click.echo("%.{0}f".format(digits) % expansao2)
+    click.echo("%.{0}f".format(digits) % expansao3)
+    click.echo("%.{0}f".format(digits) % expansao4)
+
+
+
+
 
 
 if __name__ == "__main__":
