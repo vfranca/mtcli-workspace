f2c94304d7680231416746bfa26165a300536f0b
Valmir França <vfranca3@gmail.com>
2025-01-04 15:57:35 -0300

    Adicionada marcacao e de entrada

--- mtcliplus/mtcli/e.bat
+++ mtcliplus/mtcli/e.bat
@@ -1,17 +1,9 @@
 @echo off
-rem pasta mtcli
-rem Copyright 2021-2023 Valmir França da Silva
-rem https://github.com/vfranca/
-rem calcula o retorno/risco da operação
-rem
-rem sem entrada
-rem
-if "%~1" == "" (
-gr t %E% -sl %SL% -tp %TG%
+rem mtcli
+rem marcações
+rem entry
+if "%1" == "" (
+echo %E%
 goto :EOF
-) 
-rem
-rem com entrada
-rem
+)
 set e=%1
-gr t %1 -sl %SL% -tp %A%
