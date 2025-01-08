08a5bdc3fab7a8ba1287d0903ea8cbc662a336d0
Valmir França <vfranca3@gmail.com>
2025-01-08 06:11:42 -0300

    Adicionada projecao a pb.py

--- mtcliplus/mtcli/BIN/pb.py
+++ mtcliplus/mtcli/BIN/pb.py
@@ -1,6 +1,7 @@
 """
-Calcula range e pullbacks da perna
+calcula níveis de pullback
 """
+
 import click
 import os
 
@@ -12,33 +13,35 @@ digits = os.getenv("DIGITOS")
 @click.argument("end", type=float)
 @click.argument("begin", type=float)
 def lg(begin, end):
-    """Calcula range e pullbacks da perna"""
+    """Calcula niveis de pullback"""
     leg = end - begin
-    pullback1 = end - leg * 0.33
-    pullback2 = end - leg * 0.5
-    pullback3 = end - leg * 0.66
-
     click.echo("%.{0}f".format(digits) % abs(leg))
 
-    click.echo("recuos")
-    click.echo("%.{0}f".format(digits) % pullback1)
-    click.echo("%.{0}f".format(digits) % pullback2)
-    click.echo("%.{0}f".format(digits) % pullback3)
+    pullback1 = end - leg * 0.23
+    pullback2 = end - leg * 0.33
+    pullback3 = end - leg * 0.5
+    pullback4 = end - leg * 0.62
+    pullback5 = end - leg * 0.75
 
-    expansao1 = end + leg *0.33
-    expansao2 = end + leg * 0.50
-    expansao3 = end + leg *0.66
-    expansao4 = end + leg * 1
+    click.echo("%.{0}f 23%%".format(digits) % pullback1)
+    click.echo("%.{0}f 33%%".format(digits) % pullback2)
+    click.echo("%.{0}f 50%%".format(digits) % pullback3)
+    click.echo("%.{0}f 62%%".format(digits) % pullback4)
+    click.echo("%.{0}f 75%%".format(digits) % pullback5)
 
-    click.echo("expansoes")
-    click.echo("%.{0}f".format(digits) % expansao1)
-    click.echo("%.{0}f".format(digits) % expansao2)
-    click.echo("%.{0}f".format(digits) % expansao3)
-    click.echo("%.{0}f".format(digits) % expansao4)
+    projecao = end + leg
+    click.echo("%.{0}f MM".format(digits) % projecao)
 
+    # expansao1 = end + leg *0.33
+    # expansao2 = end + leg * 0.50
+    # expansao3 = end + leg *0.66
+    # expansao4 = end + leg * 1
 
-
-
+    # click.echo("expansoes")
+    # click.echo("%.{0}f".format(digits) % expansao1)
+    # click.echo("%.{0}f".format(digits) % expansao2)
+    # click.echo("%.{0}f".format(digits) % expansao3)
+    # click.echo("%.{0}f".format(digits) % expansao4)
 
 
 if __name__ == "__main__":
