6f3118a31dfa2632cd1e9ab53b5ac5a40d4e17af
Valmir França <vfranca3@gmail.com>
2025-02-23 17:59:56 -0300

    Alterado o comano para build

--- build.bat
+++ build.bat
@@ -2,12 +2,5 @@
 set fzip=mtcli-ws.zip
 set dir=mtcli-ws
 set dir_dist=dist
-rem set dirdest1="G:\Meu Drive"
-rem set dirdest2=%HOMEPATH%
-rem set dirdest3=%SYSTEMDRIVE%
-call rar a %fzip% %dir%
+call zip -r %fzip% %dir%
 move %fzip% %dir_dist%\%fzip%
-rem copy %fzip% %dirdest2%\%fzip%
-rem copy %fzip% %dirdest3%\%fzip%
-rem del %fzip%
-
