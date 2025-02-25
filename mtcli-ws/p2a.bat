a72697699d2025206a0b700f03ae8612c5bf5b10
Valmir França <vfranca3@gmail.com>
2025-02-25 18:32:08 -0300

    Deletados arquivos obsoletos

--- mtcli-ws/p2a.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem pasta mtcli
-rem Copyright 2021-2023 Valmir França da Silva
-rem https://github.com/vfranca/
-rem perna 1 = perna 2
-rem requer pa-trading
-rem pip install pa-trading
-pa p2 %H% %L% %PBK%
