a3f801d573378da7176a1d2969f74fe01b5114a5
Valmir França <vfranca3@gmail.com>
2025-02-26 08:22:23 -0300

    Adicionados templates b3

---
+++ mtcli-ws/TEMPLATES/tibov.bat
@@ -0,0 +1,9 @@
+@echo off
+call s IBOV 0
+call ala 240
+call tam 0.2
+call lot 1
+call r 200
+call rr 3
+call coy 130000
+call moeda BRL
---
+++ mtcli-ws/TEMPLATES/tisae.bat
@@ -0,0 +1,8 @@
+@echo off
+call s ISAE4 2
+call lot 200
+call ala 125
+call r 0.05
+call rr 3
+call tam 1
+call moeda BRL
