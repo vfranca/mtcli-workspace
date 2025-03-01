d01d6c4cd88e64e02613249a6844ebfa29e78eaf
Valmir França <vfranca3@gmail.com>
2025-03-01 06:07:52 -0300

    Reformulados exibicao de marcacoes

--- mtcli-ws/prc.bat
+++ mtcli-ws/prc.bat
@@ -2,16 +2,29 @@
 rem mtcli
 rem configurações de price action
 call mtcli
+echo ativo
 call s
+echo maxima de ontem
 call hoy
+echo minima de ontem
 call loy
+echo fechamento de ontem
 call coy
+echo abertura de hoje
 call ood
+echo maxima
 call h
+echo minima
 call l
+echo movimento projetado acima
 call mmu
+echo movimento projetado abaixo
 call mmd
+echo magneto acima
 call mga
+echo movimento projetado abaixo
 call mgb
+echo linha de tendencia
 call tl
+echo linha de canal
 call tcl
--- mtcli-ws/sum.bat
+++ mtcli-ws/sum.bat
@@ -1,5 +1,8 @@
 @echo off
+echo ativo
 call s
+echo periodo
 call p
+echo data
 call dte
 call intra
