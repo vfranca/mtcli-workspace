938d2dc380c2c7f3380ad453104e61ecf39fdda1
Valmir França <vfranca3@gmail.com>
2025-04-01 03:37:28 -0300

    Adicionados ajuda e comando ao f.bat

--- mtcli-ws/f.bat
+++ mtcli-ws/f.bat
@@ -1,4 +1,15 @@
 @echo off
 rem mtcli
 rem fechamento da barra atual
+if /i "%1" == "/?" goto :help
+if /i "%1" == "/c" goto :command
+
 mt bars %SYMBOL% --view c --count 1 --period %PERIOD%
+
+:command
+echo mt bars %SYMBOL% --view c --count 1 --period %PERIOD%
+goto :EOF
+
+:help
+echo fechamento da barra atual
+goto :EOF
