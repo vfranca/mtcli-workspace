c936b29da692a5615f9ce7c36e557812c186d505
Valmir França <vfranca3@gmail.com>
2025-04-24 04:12:51 -0300

    Alterado gb.bat para registro de gabaritos de price action

--- mtcli-ws/gb.bat
+++ mtcli-ws/gb.bat
@@ -3,13 +3,20 @@ rem GABARITO DE PRICE ACTION
 if not exist GB (
 mkdir GB
 )
-set gabarito=GB\GABARITO-%Y%-%M%-%I%.txt
+set gabarito=GB\GABARITO-%SYMBOL%-%Y%-%M%-%I%.txt
 if "%1" == "0" (
 echo GABARITO %SYMBOL% %I% %M% %Y%>%gabarito%
 goto :EOF
 )
+rem exibe o conteúdo do gabarito do dia
 if "%1" == "" (
 type %gabarito%
 goto :EOF
 )
+rem lista os gabaritos salvos
+if /i "%1" == "l" (
+dir/b gb
+goto :EOF
+)
+rem registra um conteúdo
 echo "%*" >>%gabarito%
