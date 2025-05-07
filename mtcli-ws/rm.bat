a3b85c40bdbe323cc66a9def64c820482889580a
Valmir França <vfranca3@gmail.com>
2025-05-06 23:37:18 -0300

    Alterado padrao de barras para o atalho rm.bat

---
+++ mtcli-ws/rm.bat
@@ -0,0 +1,14 @@
+@echo off
+rem mtcli
+rem Copyright 2021 Valmir França
+rem atalhos de comando
+rem range médio  das barras
+if "%1" == "" (
+set count=10
+goto :range
+)
+set count=%1
+goto :range
+
+:range
+mt rm %SYMBOL% --period %PERIOD% --count %count%
